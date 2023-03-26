314edc9ca52ed19059ac1b54af25093c66f1638e
jlsherrod <74885038+jlsherrod@users.noreply.github.com>
2023-03-26 10:07:26 -0500

    added broken symlink finder to dotfiles.sh

--- dotfiles.sh
+++ dotfiles.sh
@@ -13,3 +13,11 @@ rcup -v -x "*.sh" -x "*.md" -x "iterm/com*" -x "config/karabiner"
 # must symlink the directory for these apps
 ln -sf "$HOME"/.dotfiles/iterm "$HOME"/.config
 ln -sf "$HOME"/.dotfiles/config/karabiner "$HOME"/.config
+
+echo ""
+echo "Cleaning up broken symlinks..."
+echo ""
+find -L "$HOME" -maxdepth 1 -type l -exec rm -i {} \;
+find -L "$HOME/.bin" -type l -exec rm -i {} \;
+find -L "$HOME/.config" -type l -exec rm -i {} \;
+find -L "$HOME/.zsh" -type l -exec rm -i {} \;
