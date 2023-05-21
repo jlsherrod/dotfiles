ab39a340f743e2d156e56be374786094f7090396
jlsherrod <74885038+jlsherrod@users.noreply.github.com>
2023-05-21 09:08:19 -0500

    add <Leader>y command to copy to clipboard

--- config/nvim/lua/jam/keymaps.lua
+++ config/nvim/lua/jam/keymaps.lua
@@ -2,7 +2,8 @@
 -- Keymaps for better default experience
 -- See `:help vim.keymap.set()`
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
-
+-- copy to system clipbaord
+vim.keymap.set({ 'n', 'v' }, '<Leader>y', '"+y')
 -- Remap for dealing with word wrap
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
