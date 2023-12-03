05c21bf74aebe435600603045e5e31b5af7fddf7
jlsherrod <74885038+jlsherrod@users.noreply.github.com>
2023-12-03 13:18:53 -0600

    rewrite install.sh to use homebrew to install everything. add 'mas' to install from Mac App Store. Create Brewfile. Update plugins to work with nvim.

---
+++ Brewfile
@@ -0,0 +1,76 @@
+# setup shell and core tools first
+brew "git"
+brew "git-lfs"
+brew "wget"
+brew "zsh"
+brew "node"
+
+# everything else
+brew "ack"
+brew "bandwhich"
+brew "bat"
+brew "bottom"
+brew "dust"
+brew "exa"
+brew "fd"
+brew "fzf"
+brew "gh"
+brew "diff-so-fancy"
+brew "jq"
+brew "lf"
+brew "rcm"
+brew "rg"
+brew "starship"
+brew "tealdeer"
+brew "trash"
+brew "tree"
+brew "universal-ctags"
+brew "yq"
+brew "zsh-autosuggestions"
+brew "zsh-syntax-highlighting"
+
+tap "jesseduffield/lazygit"
+brew "lazygit"
+
+tap "homebrew/cask-fonts"
+cask "font-fira-code-nerd-font"
+
+cask "1password"
+cask "1password-cli"
+cask "bartender"
+cask "brave-browser"
+cask "dash"
+cask "discord"
+cask "karabiner-elements"
+cask "keybase"
+cask "little-snitch"
+cask "notion"
+cask "obsidian"
+cask "rapidapi"
+cask "raspberry-pi-imager"
+cask "raycast"
+cask "slack"
+cask "spacelauncher"
+cask "tableplus"
+cask "trailer"
+cask "tuple"
+cask "visual-studio-code"
+cask "zoom"
+
+brew "ssh-copy-id"
+
+tap "koekeishiya/formulae"
+brew "skhd"
+brew "yabai"
+
+tap "homebrew/autoupdate"
+
+brew "mas"
+
+mas "1Password for Safari", id: 1569813296
+mas "Color Picker", id: 1545870783
+mas "Dynamic Wallpaper", id: 1453504509
+mas "Keynote", id: 409183694
+mas "Numbers", id: 409203825
+mas "Pages", id: 409201541
+mas "Pastebot", id: 1179623856
--- install.sh
+++ install.sh
@@ -1,21 +1,43 @@
 #!/bin/bash
 
-source utils.sh
+macOS="macOS"
+ubuntu="ubuntu"
+uname=$(uname -v)
 
-createPrivateFiles() {
-	touch "$HOME"/.private
+case $uname in
+  *Darwin*)
+    os=$macOS
+    ;;
+  *Ubuntu*)
+    os=$ubuntu
+    ;;
+  *)
+    echo "ERROR: Don't know how to handle this OS"
+    exit 1
+esac
+
+verifyPrivateFileExists() {
+  if [[ ! -f "$HOME/.private" ]]; then
+    echo "$HOME/.private file must be present to continue..."
+    exit 1
+  fi
 }
 
-installGit() {
-	installOrUpdate "git"
+installHomebrew() {
+  if ! command -v brew >/dev/null 2>&1; then
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    if [ -d "/opt/homebrew" ]; then 
+      eval "$(/opt/homebrew/bin/brew shellenv)"
+    elif [ -d "/home/linuxbrew/.linuxbrew" ]; then
+      eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+    fi
+  else
+    brew update
+  fi
 }
 
-installLazygit() {
-  case $os in
-    $macOS*)
-      installOrUpdate "jesseduffield/lazygit/lazygit"
-      ;;
-  esac
+setupDotFiles() {
+  (cd "$HOME"/.dotfiles || exit; bash dotfiles.sh)
 }
 
 installRust() {
@@ -26,219 +48,50 @@ installRust() {
   fi
 }
 
-installKarabinerElements() {
+installHomebrewPackages() {
   case $os in
     $macOS*)
-      installOnceFromCask "karabiner-elements"
-      ;;
-  esac
-}
-
-installDropbox() {
-  case $os in
-    $macOS*)
-      installOnceFromCask "dropbox"
-      ;;
-  esac
-}
-
-installZsh() {
-  installOrUpdate "zsh"
-
-  if [[ $SHELL != *zsh* ]]; then
-    sudo chsh -s "$(command -v zsh)" "$(whoami)"
-  fi
-}
-
-installZshSyntaxHighlighting() {
-  installOrUpdate "zsh-syntax-highlighting"
-}
-
-installZoxide() {
-  installOrUpdate "zoxide"
-}
-
-installReadline() {
-  case $os in
-    $macOS*)
-      installOrUpdate "readline"
-      ;;
-    $ubuntu*)
-      installOrUpdate "readline8"
-      ;;
-  esac
-}
-
-installLess() {
-  installOrUpdate "less"
-}
-
-installFzf() {
-  installOrUpdate "fzf"
-}
-
-installRg() {
-  case $os in
-    $macOS*)
-      installOrUpdate "ripgrep"
-      ;;
-    $ubuntu*)
-      # https://github.com/sharkdp/bat/issues/938
-      sudo apt -y install -o Dpkg::Options::="--force-overwrite" ripgrep
-      ;;
-  esac
-
-}
-
-installGh() {
-  case $os in
-    $macOS*)
-      installOrUpdate "gh"
-      ;;
-    $ubuntu*)
-      if ! command -v gh >/dev/null 2>&1; then
-        curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo gpg --dearmor -o /usr/share/keyrings/githubcli-archive-keyring.gpg
-        echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
-        updateAvailablePackages
-      fi
-
-      installOrUpdate "gh"
-
-      gh completion -s zsh | sudo tee /usr/local/share/zsh/site-functions/_gh > /dev/null
-      ;;
-  esac
-
-  gh extension install dlvhdr/gh-dash
-}
-
-installTerminal() {
-  case $os in
-    $macOS*)
-      installOnceFromCask "iterm2"
-      ;;
-  esac
-}
-
-installWindowManager() {
-  case $os in
-    $macOS*)
-      if ! command -v yabai >/dev/null 2>&1; then
-        installOrUpdate "koekeishiya/formulae/yabai"
-
-        echo -e "You need to disable SIP and do some other hackery to get this loading on macOS"
-        echo -e "https://github.com/koekeishiya/yabai/wiki/Installing-yabai-(latest-release)"
-        echo -e "Type 'OK' to acknowledge this!"
-        read -r confirm
-        if [ "$confirm" != "OK" ] ; then
-          echo "Get it done! Exiting."
-          exit 1
-        fi
+      echo -e "Make sure you've launched the Mac App Store and signed into your accounts!"
+      echo -e "Type 'done' when you're ready to continue."
+      read -r confirm
+      if [ "$confirm" != "done" ] ; then
+        echo "Glad I asked! Bye."
+        exit 1
       else
-        brew services stop yabai
-        brew upgrade yabai
-        brew services start yabai
-
-        sudo yabai --uninstall-sa
-        sudo yabai --install-sa
-
-        killall Dock
+        ##Accept xcode license for multiple items in homebrew
+        sudo xcodebuild -license accept
+        brew bundle
       fi
-      ;;
-  esac
-}
-
-installTree() {
-  installOrUpdate "tree"
-}
-
-installWget() {
-  installOrUpdate "wget"
-}
-
-installTrash() {
-  case $os in
-    $macOS*)
-      installOrUpdate "trash"
-      ;;
-  esac
-}
-
-installBottom() {
-  case $os in
-    $macOS*)
-      installOrUpdate "bottom"
-      ;;
-  esac
-}
-
-installBandwhich() {
-  case $os in
-    $macOS*)
-      installOrUpdate "bandwhich"
-      ;;
-  esac
-}
-
-installNeovim() {
-  case $os in
-    $macOS*)
-      installOrUpdate "neovim"
       ;;
     $ubuntu*)
-      if ! command -v nvim >/dev/null 2>&1; then
-        sudo snap install nvim --classic
-      else
-        sudo snap refresh nvim --classic
-      fi
-
+      sudo timedatectl set-timezone America/Chicago
+      sudo apt-get -y update
+      sudo apt dist-upgrade -y
+      sudo apt auto-remove -y
       ;;
   esac
+
+  brew cleanup
 }
 
-setupNeovim() {
-  mkdir -p "$HOME"/.config/nvim/
-
-  if [ ! -f "$HOME"/.local/share/nvim/site/pack/packer/start/packer.nvim ]; then
-    pip3 install neovim
-    yarn global add neovm
-  else
-    pip3 install neovim --upgrade
-    yarn global upgrade neovm
-  fi
-
-  nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
+miscellaneous() {
+  yabai --install-service >/dev/null 2>&1;
+  skhd --install-service >/dev/null 2>&1;
+  yabai --restart-service
+  skhd --restart-service
+  yes | "$(brew --prefix)"/opt/fzf/install >/dev/null 2>&1;
 }
 
 echo ""
 echo "Running installation for $os..."
 echo ""
 
-createPrivateFiles
-installGh
-installGit
-installLazygit
+verifyPrivateFileExists
+installHomebrew
+installHomebrewPackages
+setupDotFiles
 installRust
-installKarabinerElements
-installDropbox
-installZsh
-installZshSyntaxHighlighting
-installZoxide
-installReadline
-installLess
-installFzf
-installRg
-installGh
-installTerminal
-installWindowManager
-installTree
-installWget
-installTrash
-installBottom
-installBottom
-installBandwhich
-installNeovim
-setupNeovim
-cleanupPackages
+miscellaneous
 
 echo ""
 echo "Done! You'll probably need to restart your shell/SSH session..."
