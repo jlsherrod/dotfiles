beb1263d808d05eddfc82fe504cba37b37422219
jlsherrod <74885038+jlsherrod@users.noreply.github.com>
2023-12-11 00:01:48 -0600

    add emmet and tailwind lsps

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -137,6 +137,15 @@ function M.setup()
 			end,
 		},
 		{ "jose-elias-alvarez/null-ls.nvim" },
+		{
+			"roobert/tailwindcss-colorizer-cmp.nvim",
+			-- optionally, override the default options:
+			config = function()
+				require("tailwindcss-colorizer-cmp").setup({
+					color_square_width = 2,
+				})
+			end,
+		},
 	}, {})
 end
 
