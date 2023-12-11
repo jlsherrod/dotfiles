c1abffa19f6fc02fdb3c89006c49776db22facb5
jlsherrod <74885038+jlsherrod@users.noreply.github.com>
2023-12-11 00:01:10 -0600

    change emmet-ls to emmet-language-server

--- config/nvim/lua/jam/lsp/emmet.lua
+++ config/nvim/lua/jam/lsp/emmet.lua
@@ -1,15 +1,7 @@
-local lspconfig = require('lspconfig')
+local lspconfig = require("lspconfig")
 
-lspconfig.emmet_ls.setup({
-  on_attach = require('jam.lsp').on_attach,
-  capabilities = require('jam.lsp').capabilities(),
-  filetypes = { 'html', 'typescriptreact', 'javascriptreact', 'css', 'sass', 'scss', 'less' },
-  init_options = {
-    html = {
-      options = {
-        -- For possible options, see: https://github.com/emmetio/emmet/blob/master/src/config.ts#L79-L267
-        ['bem.enabled'] = true,
-      },
-    },
-  },
+lspconfig.emmet_language_server.setup({
+	on_attach = require("jam.lsp").on_attach,
+	capabilities = require("jam.lsp").capabilities(),
+	filetypes = { "html", "typescriptreact", "javascriptreact", "css", "sass", "scss", "less" },
 })
